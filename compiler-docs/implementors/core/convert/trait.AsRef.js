62a522312ac06c1be62068b085827abb56207c66
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2021-10-06 18:17:40 +0200

    Generated website

---
+++ compiler-docs/implementors/core/convert/trait.AsRef.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["fe_analyzer"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.54.0/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.54.0/std/primitive.str.html\">str</a>&gt; for <a class=\"enum\" href=\"fe_analyzer/builtins/enum.ReservedName.html\" title=\"enum fe_analyzer::builtins::ReservedName\">ReservedName</a>","synthetic":false,"types":["fe_analyzer::builtins::ReservedName"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
